3e6381f8b9ee39d41f6cd90430dc5ad757d6cae5
Henri Niva <henri.a.niva@gmail.com>
2018-09-30 20:35:13 +0300

    Parsing works

--- game_state.py
+++ game_state.py
@@ -1,4 +1,5 @@
 STACK_COUNT = 8
+INITIAL_STACK_SIZE = 5
 OPEN_SLOT_COUNT = 3
 SUIT_STACK_COUNT = 3
 
@@ -8,10 +9,12 @@ SUIT_STACK_COUNT = 3
 # Suit token cards are represented as a 2-tuple, (color, 0)
 # Open slots contain the cards, or a 2-tuple (color, -1) if the token stack has been turned over there
 
+
 class GameState:
-    stacks = [] # list of lists of tuples
-    open_slots = [] # list of tuples
-    suit_stacks = [] # list of lists (2-tuples really) indicating current value per suit
+    stacks = []  # list of lists of tuples
+    open_slots = []  # list of tuples
+    # list of lists (2-tuples really) indicating current value per suit
+    suit_stacks = []
 
     def __init__(self):
         # Initialize all stacks as empty
@@ -24,12 +27,16 @@ class GameState:
         self.suit_stacks.append(["black", 0])
 
     def clone(self):
+        """
+            Clones the given GameState object
+        """
         clone = GameState()
 
-        for i in range(STACK_COUNT): 
+        for i in range(STACK_COUNT):
             for j in range(len(self.stacks[i])):
-                clone.stacks[i].append(self.stacks[i][j]) # Copy each card from each stack
-        
+                # Copy each card from each stack
+                clone.stacks[i].append(self.stacks[i][j])
+
         for i in range(len(self.open_slots)):
             clone.open_slots.append(self.open_slots[i])
 
@@ -38,27 +45,31 @@ class GameState:
 
         return clone
 
-    # Determine if the current state is the won end state
     def is_won(self):
+        """
+            Determine if the current state is the won end state
+        """
         for i in range(len(self.open_slots)):
             if self.open_slots[i][1] != -1:
                 return False
-        
+
         for i in range(SUIT_STACK_COUNT):
             if self.suit_stacks[i][1] != 9:
                 return False
-        
+
         for i in range(STACK_COUNT):
             if len(self.stacks[i]) != 0:
                 return False
 
         return True
 
-    # Automatically resolve state transformations that are "free" e.g. removing the rose card and suit cards such that
-    # a card is only moved if all off-suit suit stacks contain at least a value that is 1 less than the card being moved
-    # Example: suit stacks 000, can move any 1's and 2's
-    # Example: suit stacks 412, can move middle color (-> 422) but not others (-> 512 or 413)
     def auto_resolve(self):
+        """
+            Automatically resolve state transformations that are "free" e.g. removing the rose card and suit cards such that
+            a card is only moved if all off-suit suit stacks contain at least a value that is 1 less than the card being moved
+            Example: suit stacks 000, can move any 1's and 2's
+            Example: suit stacks 412, can move middle color (-> 422) but not others (-> 512 or 413)
+        """
         previous_count = 0
         while previous_count != self.get_total_card_count():
             previous_count = self.get_total_card_count()
@@ -79,11 +90,11 @@ class GameState:
                 if self.suit_index(top_card) is None:
                     self.pull_from_stack(i, 1)
                     continue
-                
+
                 current_suit_value = self.suit_stacks[self.suit_index(top_card)][1]
 
                 # If card is (current + 1) and the value is (min + 1), remove it
-                # Also remove any 1's and 2's (only of can be placed)
+                # Also remove any 1's and 2's (only of can be pl    aced)
                 if top_card[1] == current_suit_value + 1 and (top_card[1] == minimum_suit_value + 1 or top_card[1] == 1 or top_card[1] == 2):
                     self.pull_from_stack(i, 1)
                     self.suit_stacks[self.suit_index(top_card)][1] += 1
@@ -94,36 +105,42 @@ class GameState:
                 continue
 
             open_slot_card = None
-                
+
             # Also go through all open slot cards. Rose cannot be found here
             for i in range(len(self.open_slots)):
-                card = self.open_slots[len(self.open_slots) - 1 - i] # Reverse order
+                # Reverse order
+                card = self.open_slots[len(self.open_slots) - 1 - i]
                 current_suit_value = self.suit_stacks[self.suit_index(card)][1]
 
                 if card[1] == current_suit_value + 1 and (card[1] == minimum_suit_value + 1 or card[1] == 1 or card[1] == 2):
                     open_slot_card = card
                     break
-            
+
             if open_slot_card is not None:
                 self.open_slots.remove(open_slot_card)
                 self.suit_stacks[self.suit_index(card)][1] += 1
 
-
-    # Return the card that is on top of the given stack at index. Returns None if stack is empty
-    # Does not remove the card from the stack
     def query_stack_top(self, index):
+        """
+            Return the card that is on top of the given stack at index. Returns None if stack is empty
+            Does not remove the card from the stack
+        """
         stack = self.stacks[index]
         if len(stack) == 0:
             return None
         return stack[len(stack) - 1]
 
-    # Returns the total card count in the stacks
     def get_total_card_count(self):
+        """
+            Returns the total card count in the stacks
+        """
         return sum([len(x) for x in self.stacks])
 
-    # Return given number of cards from the given stack
-    # Removes the said cards from the stack
     def pull_from_stack(self, index, count):
+        """
+            Return given number of cards from the given stack
+            Removes the said cards from the stack
+        """
         stack = self.stacks[index]
         start = stack[:len(stack) - count]
         end = stack[-count:]
@@ -132,8 +149,32 @@ class GameState:
         self.stacks[index] = start
         return end
 
-    # Get the index of the given suit. Returns None for the rose card
+    def parse_card_into_stack(self, index, card):
+        """
+            Puts the given card at the top of the stack, used in the image parsing
+        """
+        self.stacks[index].append(card)
+
+    def initialize_state(self):
+        """
+            Tells the state that the parsing has concluded. The state needs to check how many cards it is missing in 
+            each color and update the suit_stacks counts accordingly.
+        """
+        suit_check = [[0, 0, 0] + [x for x in range(10)] for i in range(3)]
+
+        for stack_index in range(STACK_COUNT):
+            stack = self.stacks[stack_index]
+            for card_index in range(len(stack)):
+                card = stack[card_index]
+                suit_index = self.suit_index(card)
+
+                if suit_index is None:
+                    continue
+
     def suit_index(self, card):
+        """
+            Get the index of the given suit. Returns None for the rose card
+        """
         if card[0] == "red":
             return 0
         elif card[0] == "green":
@@ -146,7 +187,7 @@ class GameState:
         for i in range(STACK_COUNT):
             if self.stacks[i] != other.stacks[i]:
                 return False
-        
+
         for i in range(OPEN_SLOT_COUNT):
             if self.open_slots[i] != other.open_slots[i]:
                 return False
@@ -160,7 +201,6 @@ class GameState:
 
     def __str__(self):
         return ("Open slots: " + ", ".join(map(lambda slot: slot[0] + " " + str(slot[1]), self.open_slots)) + "\n" +
-            "Suit stacks: " + ", ".join(map(lambda slot: slot[0] + " " + str(slot[1]), self.suit_stacks)) + "\n" +
-            "Board:\n" +
-            "\n".join([", ".join(map(lambda slot: slot[0] + " " + str(slot[1]), stack)) for stack in self.stacks]))
-            
+                "Suit stacks: " + ", ".join(map(lambda slot: slot[0] + " " + str(slot[1]), self.suit_stacks)) + "\n" +
+                "Board:\n" +
+                "\n".join([", ".join(map(lambda slot: slot[0] + " " + str(slot[1]), stack)) for stack in self.stacks]))
--- solver.py
+++ solver.py
@@ -4,7 +4,7 @@ import time
 import functools
 import math
 
-from game_state import GameState
+from game_state import GameState, STACK_COUNT, OPEN_SLOT_COUNT, SUIT_STACK_COUNT, INITIAL_STACK_SIZE
 
 # Constants used to crop the game view from the whole screen
 # Works properly if game is in native resolution
@@ -36,25 +36,74 @@ BOARD_VERTICAL_DELIMITER = 31
 CARD_VALUE_OFFSET = (12, 10)
 CARD_VALUE_SIZE = (12, 21)
 
-COLOR_MATCH_THRESHOLD = 30
+COLOR_MATCH_THRESHOLD = 2
+
+# Color average lookup
+CARD_LOOKUP = {}
+CARD_LOOKUP["red"] = [
+    (190, 156, 139, 193, 195, 179),
+    (190, 157, 139, 177, 67, 44),
+    (187, 136, 117, 175, 56, 33),
+    (187, 135, 116, 175, 54, 31),
+    (186, 132, 113, 193, 194, 178),
+    (186, 134, 115, 179, 88, 66),
+    (186, 133, 114, 191, 177, 161),
+    (188, 143, 125, 180, 92, 70),
+    (185, 125, 105, 175, 57, 33),
+    (186, 133, 114, 175, 55, 31)
+]
+CARD_LOOKUP["green"] = [
+    (121, 160, 137, 148, 173, 152),
+    (148, 173, 152, 45, 123, 91),
+    (123, 161, 138, 32, 117, 83),
+    (122, 161, 137, 30, 116, 82),
+    (119, 159, 135, 193, 194, 178),
+    (121, 160, 136, 69, 134, 105),
+    (119, 159, 136, 173, 185, 167),
+    (132, 165, 143, 74, 137, 108),
+    (110, 155, 130, 32, 117, 83),
+    (120, 160, 136, 30, 116, 82)
+]
+CARD_LOOKUP["black"] = [
+    (131, 132, 122, 68, 69, 63),
+    (147, 148, 136, 30, 30, 27),
+    (120, 121, 111, 16, 16, 14),
+    (119, 120, 110, 13, 13, 12),
+    (115, 116, 107, 193, 194, 178),
+    (117, 118, 109, 56, 56, 52),
+    (116, 117, 108, 171, 172, 158),
+    (129, 130, 120, 61, 62, 57),
+    (106, 106, 98, 16, 16, 15),
+    (116, 117, 108, 13, 14, 12)
+]
+CARD_LOOKUP["rose"] = [(171, 142, 121, 193, 195, 179)]
+
 
 def main():
     intro_print()
     # time.sleep(5)
-    
+
     solve()
 
+
 def intro_print():
+    """
+        Prints introductory test of the program's features
+    """
     print("Launching in 5 seconds")
     print("Make sure the game is opened in fullscreen on the main window")
     print("To exit, close the script between games")
 
+
 def solve():
-    #image = ImageGrab.grab()
-    #image = crop(image)
+    """
+        Solves the current game configuration
+    """
+    # image = ImageGrab.grab()
+    # image = crop(image)
     image = PIL.Image.open("reference_img.bmp")
 
-    # Initialize beginning game state
+    # Initialize beginning game statepip
     state = GameState()
 
     # Parse the image and populate the state
@@ -64,36 +113,14 @@ def solve():
 
     print(state)
 
-    color_set = {}
-    
-    # TEMP get all values, save as image with the color as filename
-    for i in range(8):
-        for j in range(5):
-            left = BOARD_TOP_LEFT[0] + i * BOARD_HORIZONTAL_DELIMITER + CARD_VALUE_OFFSET[0]
-            top = BOARD_TOP_LEFT[1] + j * BOARD_VERTICAL_DELIMITER + CARD_VALUE_OFFSET[1]
-            right = left + CARD_VALUE_SIZE[0]
-            bottom = top + CARD_VALUE_SIZE[1]
-            card_value = image.crop((left, top, right, bottom))
-            pixels = list(card_value.getdata())
-            avg_color = avg_color_list(pixels)
 
-            if (avg_color in color_set):
-                print("Clash", i, j, color_set[avg_color])
-
-            color_set[avg_color] = (i, j)
-
-            fname = str(i) + "_" + str(j) + "_" + "_".join(map(str, avg_color)) + ".bmp"
-            #card_value.save(fname)
-
-    print(color_set)
-    print(len(color_set))
-
-
-# Crop the image to only contain the game view
-# Define the game view as a defined rectangle around the center of the screen
-# Assume the game is in native resolution
-# Confirmed to work in 1080p and 1440p
 def crop(image):
+    """
+        Crop the image to only contain the game view
+        Define the game view as a defined rectangle around the center of the screen
+        Assume the game is in native resolution
+        Confirmed to work in 1080p and 1440p
+    """
     width = image.size[0]
     height = image.size[1]
     game_width = GAME_WIDTH
@@ -104,13 +131,78 @@ def crop(image):
 
     return image.crop((game_left, game_top, width - game_left, height - game_top))
 
-# Parse the image and populate the given game state
-def populate_state(image, state):
-    pass
 
-# Sample an average color from the image at the given position
-# Averages a 3x3 kernel around the pixel
+def populate_state(image, state):
+    """
+        Parse the image and populate the given game state
+    """
+
+    sampled_colors = {}
+
+    # Loop through the board and extract color data from card values
+    for i in range(STACK_COUNT):
+        for j in range(INITIAL_STACK_SIZE):
+            left = BOARD_TOP_LEFT[0] + i * BOARD_HORIZONTAL_DELIMITER + CARD_VALUE_OFFSET[0]
+            top = BOARD_TOP_LEFT[1] + j * BOARD_VERTICAL_DELIMITER + CARD_VALUE_OFFSET[1]
+            right = left + CARD_VALUE_SIZE[0]
+            bottom = top + CARD_VALUE_SIZE[1]
+            card_value = image.crop((left, top, right, bottom))
+
+            # Get avg from top left corner
+            top_left_avg = sample_avg_color(card_value, (2, 2))
+
+            # Get overall color average
+            pixels = list(card_value.getdata())
+            avg_color = avg_color_list(pixels)
+
+            comb_color = avg_color + top_left_avg
+
+            sampled_colors[(i, j)] = comb_color
+
+    position_lookup = {}
+
+    # Find the card colors and values
+    for position in sampled_colors:
+        comb_color = sampled_colors[position]
+
+        found = False
+
+        # Go through all color settings and try to find the correct one, that is as close to the given values as possible
+        for suit in CARD_LOOKUP:
+            if found:
+                break
+            suit_cards = CARD_LOOKUP[suit]
+            for card_index in range(len(suit_cards)):
+                comparison_color = suit_cards[card_index]
+
+                # Split the 6-tuples into 3-tuples
+                sampled_avg_color = comb_color[:3]
+                sampled_check_color = comb_color[3:]
+
+                comparison_avg_color = comparison_color[:3]
+                comparison_check_color = comparison_color[3:]
+
+                # Test if the colors are close to each other. Store the card position
+                if color_distance(sampled_avg_color, comparison_avg_color) < COLOR_MATCH_THRESHOLD and color_distance(sampled_check_color, comparison_check_color) < COLOR_MATCH_THRESHOLD:
+                    position_lookup[position] = (suit, card_index)
+                    found = True
+                    break
+
+        if not found:
+            pass
+            # The card that should have been at the given place was probably auto-resolved. The state will update rose + suit counts
+            # print("Not found", position, comb_color)
+
+    for position in sorted(position_lookup.keys()):
+        stack_index = position[0]
+        state.parse_card_into_stack(stack_index, position_lookup[position])
+
+
 def sample_avg_color(image, position):
+    """
+        Sample an average color from the image at the given position
+        Averages a 3x3 kernel around the pixel
+    """
     kernel = []
     topleft = (position[0] - 1, position[1] - 1)
     for i in range(3):
@@ -118,14 +210,22 @@ def sample_avg_color(image, position):
             kernel.append(image.getpixel((topleft[0] + j, topleft[1] + i)))
     return avg_color_list(kernel)
 
-# Returns the average color of 
+
 def avg_color_list(color_list):
+    """
+        Returns the average color of the given list of 3-tuples
+    """
     colors = tuple(functools.reduce(lambda x, y: tuple(map(lambda a, b: a + b, x, y)), color_list))
     colors = tuple(map(lambda x: x // len(color_list), colors))
     return colors
 
+
 def color_distance(from_color, to_color):
+    """
+        Calculate the euclidean distance of two colors in 3D space
+    """
     return math.sqrt((from_color[0] - to_color[0]) ** 2 + (from_color[1] - to_color[1]) ** 2 + (from_color[2] - to_color[2]) ** 2)
+
 
 if __name__ == "__main__":
     main()
